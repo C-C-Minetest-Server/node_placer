5657bb9f964a963f25be7b56cc1a42847353d9cc
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-04-24 07:14:27 +0800

    init

---
+++ init.lua
@@ -0,0 +1,43 @@
+local MN = minetest.get_current_modname()
+local MP = minetest.get_modpath(MN)
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+  if not(placer and placer:is_player()) then return end
+  if (newnode.name == "air") then return end
+  local pname = placer:get_player_name()
+  local nmeta = minetest.get_meta(pos)
+  nmeta:set_string("np_placer",pname)
+  nmeta:set_int("np_time",os.time())
+end)
+
+
+local function on_place(itemstack, placer, pointed_thing)
+  if not(placer and placer:is_player()) then return end
+  if not pointed_thing then return end
+  local ppos = minetest.get_pointed_thing_position(pointed_thing)
+  local nmeta = minetest.get_meta(ppos)
+  local pname = placer:get_player_name()
+  local np_placer = nmeta:get_string("np_placer")
+  local np_time_int = nmeta:get_int("np_time")
+  local RSTR
+  if not(np_placer and np_time_int) or np_placer == "" then
+    RSTR = "unknown."
+  else
+    RSTR = np_placer .. ", placed at " .. os.date("%m/%d/%y %H:%M:%S %z",np_time_int) .. "."
+  end
+  minetest.chat_send_player(pname,"The placer of this node is " .. RSTR)
+end
+
+minetest.register_craftitem("node_placer:check_tool",{
+  description = "Node Placer Checking Tool",
+  inventory_image = "halo.png^search.png^fast_btn.png",
+  on_place = on_place,
+})
+
+minetest.register_craftitem("node_placer:check_tool_liquid",{
+  description = "Node Placer Checking Tool (Liquid Pointable)",
+  inventory_image = "halo.png^search.png^fast_btn.png^bubble.png",
+  liquids_pointable = true,
+  on_place = on_place,
+})
+
+dofile(MP .. "/bucket.lua")
