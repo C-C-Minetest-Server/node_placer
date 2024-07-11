599e56296177fadee316efbd1d911a3441ca3b84
1F616EMO <root@1f616emo.xyz>
2024-07-11 18:51:59 +0800

    Code tidyup

--- init.lua
+++ init.lua
@@ -5,15 +5,20 @@
 
 local MN = minetest.get_current_modname()
 local MP = minetest.get_modpath(MN)
+local S = minetest.get_translator("node_placer")
+
 node_placer = {}
+
 node_placer.set_placer = function(pos, name)
 	local nmeta = minetest.get_meta(pos)
 	nmeta:set_string("np_placer", name)
 	nmeta:set_int("np_time", os.time())
+	nmeta:mark_as_private({ "np_placer", "np_time" })
 end
+
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
 	if not (placer and placer:is_player()) then return end
-	if (newnode.name == "air") then return end
+	if newnode.name == "air" then return end
 	local pname = placer:get_player_name()
 	node_placer.set_placer(pos, pname)
 end)
@@ -27,23 +32,22 @@ local function on_place(itemstack, placer, pointed_thing)
 	local pname = placer:get_player_name()
 	local np_placer = nmeta:get_string("np_placer")
 	local np_time_int = nmeta:get_int("np_time")
-	local RSTR
 	if not (np_placer and np_time_int) or np_placer == "" then
-		RSTR = "unknown."
+		minetest.chat_send_player(pname, S("The placer of this node is unknown."))
 	else
-		RSTR = np_placer .. ", placed at " .. os.date("%m/%d/%y %H:%M:%S %z", np_time_int) .. "."
+		minetest.chat_send_player(pname, "The placer of this node is @1, placed on @2.",
+			np_placer, os.date("%m/%d/%y %H:%M:%S %z", np_time_int))
 	end
-	minetest.chat_send_player(pname, "The placer of this node is " .. RSTR)
 end
 
 minetest.register_craftitem("node_placer:check_tool", {
-	description = "Node Placer Checking Tool",
+	description = S("Node Placer Checking Tool"),
 	inventory_image = "halo.png^search.png^fast_btn.png",
 	on_place = on_place,
 })
 
 minetest.register_craftitem("node_placer:check_tool_liquid", {
-	description = "Node Placer Checking Tool (Liquid Pointable)",
+	description = S("Node Placer Checking Tool (Liquid Pointable)"),
 	inventory_image = "halo.png^search.png^fast_btn.png^bubble.png",
 	liquids_pointable = true,
 	on_place = on_place,
