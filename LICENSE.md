3a739ce4df5160523e957eeffd8a69ced03d900d
1F616EMO <root@1f616emo.xyz>
2024-07-11 19:00:43 +0800

    Code quality

--- LICENSE.md
+++ LICENSE.md
@@ -17,3 +17,11 @@
         You should have received a copy of the GNU Lesser General Public
         License along with this library; if not, write to the Free Software
         Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+## License of media
+
+* `node_placer_search.png`: 1F616EMO, [CC BY-SA 3.0][CCBYSA3]
+  * Search icon: paramat, [CC BY-SA 3.0][CCBYSA3]
+  * Background: Yoruma, [CC BY-SA 3.0][CCBYSA3]
+
+[CCBYSA3]: http://creativecommons.org/licenses/by-sa/3.0/
--- init.lua
+++ init.lua
@@ -3,8 +3,6 @@
 -- Copyright 2021, 2024  1F616EMO
 -- SPDX-License-Identifier: LGPL-2.1-or-later
 
-local MN = minetest.get_current_modname()
-local MP = minetest.get_modpath(MN)
 local S = minetest.get_translator("node_placer")
 
 node_placer = {}
@@ -16,7 +14,7 @@ node_placer.set_placer = function(pos, name)
 	nmeta:mark_as_private({ "np_placer", "np_time" })
 end
 
-minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+minetest.register_on_placenode(function(pos, newnode, placer)
 	if not (placer and placer:is_player()) then return end
 	if newnode.name == "air" then return end
 	local pname = placer:get_player_name()
@@ -24,7 +22,7 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 end)
 
 
-local function on_place(itemstack, placer, pointed_thing)
+local function on_place(_, placer, pointed_thing)
 	if not (placer and placer:is_player()) then return end
 	if not pointed_thing then return end
 	local ppos = minetest.get_pointed_thing_position(pointed_thing)
@@ -32,7 +30,7 @@ local function on_place(itemstack, placer, pointed_thing)
 	local pname = placer:get_player_name()
 	local np_placer = nmeta:get_string("np_placer")
 	local np_time_int = nmeta:get_int("np_time")
-	if not (np_placer and np_time_int) or np_placer == "" then
+	if np_placer == "" or np_time_int == 0 then
 		minetest.chat_send_player(pname, S("The placer of this node is unknown."))
 	else
 		minetest.chat_send_player(pname, "The placer of this node is @1, placed on @2.",
@@ -42,13 +40,13 @@ end
 
 minetest.register_craftitem("node_placer:check_tool", {
 	description = S("Node Placer Checking Tool"),
-	inventory_image = "halo.png^search.png^fast_btn.png",
+	inventory_image = "node_placer_search.png",
 	on_place = on_place,
 })
 
 minetest.register_craftitem("node_placer:check_tool_liquid", {
 	description = S("Node Placer Checking Tool (Liquid Pointable)"),
-	inventory_image = "halo.png^search.png^fast_btn.png^bubble.png",
+	inventory_image = "node_placer_search.png^bubble.png",
 	liquids_pointable = true,
 	on_place = on_place,
 })
